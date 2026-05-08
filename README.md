6906f391986145fc9412a2db8333644dc5c1fcf3
misccpu <supertriplejump950@gmail.com>
2026-05-08 00:54:57 -0700

    Moved blurb

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ek_site
  Recreation of Databases Final Project
+Welcome to my simple Pokemon: Emerald Kaizo site! This was originally a final project for my database class,
+which I've finally retrieved and revitalized. I'm not really happy with some of the formatting, but I may find
+the time to update it someday! Please enjoy and know that I can do database things!
--- web/index.php
+++ web/index.php
@@ -19,9 +19,7 @@ TO DO:
     ?>
     
     <div>
-        Welcome to my simple Pokemon: Emerald Kaizo site! This was originally a final project for my database class,
-        which I've finally retrieved and revitalized. I'm not really happy with some of the formatting, but I may find
-        the time to update it someday! Please enjoy and know that I can do database things!
+        
     </div>
     
     <?php
